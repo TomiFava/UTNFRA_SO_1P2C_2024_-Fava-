c854b925c5e6f37729237eaf262ee633ae4b0c1d
Tomas G. Fava <tomasgfava2024@gmail.com>
2024-10-07 23:04:46 -0300

    Update Punto_C.sh

--- RTA_SCRIPT_Examen_20241007/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_C.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
-echo "Script Punto C"
+
+# 1-) Mensaje de bienvenida:
+echo "Iniciando Script 'C'..."
+
+# 2-) Función para generar contraseñas:
+generate_password() {
+    echo $(openssl passwd -crypt "$1")
+}
+CONTRASEÑA=$(generate_password 'osboxes.org')
+
+# 3-) Crear los grupos:
 sudo groupadd p1c2_2024_gAlumno
-sudo groupadd 1c2_2024_gProfesores
+sudo groupadd p1c2_2024_gProfesores
 
-sudo useradd -m -s /bin/bash -c "alumno 1" -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" -G p1c2_2024_gAlumno p1c2_2024_A1
+# 4-) Crear los usuarios con sus respectivos grupos:
+sudo useradd -m -G p1c2_2024_gAlumno -p "$CONTRASEÑA" p1c2_2024_A1
+sudo useradd -m -G p1c2_2024_gAlumno -p "$CONTRASEÑA" p1c2_2024_A2
+sudo useradd -m -G p1c2_2024_gAlumno -p "$CONTRASEÑA" p1c2_2024_A3
+sudo useradd -m -G p1c2_2024_gProfesores -p "$CONTRASEÑA" p1c2_2024_P1
 
-sudo useradd -m -s /bin/bash -c "alumno 2" -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" -G p1c2_2024_gAlumno p1c2_2024_A2
+# 5-) Verificar si el directorio ya existe
+if [ ! -d "/Examenes-UTN" ]; then
+    sudo mkdir -p /Examenes-UTN/{alumno_{1..3},profesores}
+else
+    echo "/Examenes-UTN ya existe."
+fi
 
-sudo useradd -m -s /bin/bash -c "alumno 3" -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" -G p1c2_2024_gAlumno p1c2_2024_A3
-
-sudo useradd -m -s /bin/bash -c "profesor 1" -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" -G p1c2_2024_gProfesores p1c2_2024_P1
-
-sudo chmod 750 /Examenes-UTN/alumno_1
-sudo chmod 760 /Examenes-UTN/alumno_2
-sudo chmod 700 /Examenes-UTN/alumno_3
-sudo chmod 775 /Examenes-UTN/profesores
+# 6-) Asignación de permisos a las carpetas:
 sudo chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
+sudo chmod 750 /Examenes-UTN/alumno_1
+
 sudo chown p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
+sudo chmod 740 /Examenes-UTN/alumno_2
+
 sudo chown p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chmod 750 /Examenes-UTN/alumno_3
+
 sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+sudo chmod 775 /Examenes-UTN/profesores
 
-sudo -su -c "whoami > /Examenes-UTN/alumno_1/validar1.txt" p1c2_2024_A1
-sudo -su -c "whoami > /Examenes-UTN/alumno_2/validar1.txt" p1c2_2024_A2
-sudo -su -c "whoami > /Examenes-UTN/alumno_3/validar1.txt" p1c2_2024_A3
-sudo -su -c "whoami > /Examenes-UTN/profesores/validar1.txt" p1c2_2024_P1
+# 7-) Crear un archivo de validación para cada usuario:
+for user in p1c2_2024_A1 p1c2_2024_A2 p1c2_2024_A3 p1c2_2024_P1; do
+    sudo su -c "whoami > /Examenes-UTN/${user/alumno_/alumno_1}/validar.txt" "$user"
+done
 
+# 8-) Mensaje de finalización:
+echo "Usuarios creados, permisos ajustados y archivo de validación generado."
