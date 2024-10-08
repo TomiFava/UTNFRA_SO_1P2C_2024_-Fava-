f3761efeba15bb116e485190d423d3d7925189ac
Tomas G. Fava <tomasgfava2024@gmail.com>
2024-10-07 23:03:29 -0300

    Update Punto_A.sh

--- RTA_SCRIPT_Examen_20241007/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_A.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-echo "Script Punto A"
-sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
-chmod 777 Punto_A.sh
+
+# 1-) Mensaje de bienvenida:
+echo " Ejecutando Script 'A', se están creando las estructuras de directorios para los exámenes..."
+
+# 2-) Creación de la estructura de directorios
+mkdir -p /Examenes-UTN/{alumno_1,alumno_2,alumno_3,profesores}/{parcial_1,parcial_2,parcial_3}
+
+# 3-) Listar la estructura para verificar
+tree /Examenes-UTN
+
+# 4-) Estableciendo permiso (opcional si es necesario):
+sudo chmod 764 Punto_A.sh
+
+# 5-) Mensaje de finalización:
+echo " El Script 'A' se ha ejecutado correctamente, creación de estructuras completada!"
 
